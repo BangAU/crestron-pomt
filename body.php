6b6f6f4c5ec3d4b73328556215fa02d50286fc64
francisbang <59549139+francisbang@users.noreply.github.com>
2020-01-31 09:16:07 +1100

    initial

---
+++ body.php
@@ -0,0 +1,214 @@
+<section class="top-page">
+  <header id="header">
+    <div class="menu">
+      <div class="container">
+        <nav id="nav">
+          <input type="checkbox" id="show-mobile">
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul>
+              <li><a href="#it-works">KNOW IT WORKS</a></li>
+              <li><a href="#collaborate">COLLABORATE WITH CONFIDENCE</a></li>
+              <li><a href="#performance">OUR PERFORMANCE</a></li>
+              <li><a href="#believe">BELIEVE IN CRESTRON</a></li>
+              <li><a href="#work">WORK WITH US</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="logo-container">
+      <div class="container">
+        <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/logo-crestron.png" width="177" alt="Crestron Partners"></a>
+        <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/logo-pomt.svg" width="111" alt="Ten Four"></a>
+      </div>
+    </div>
+  </header>
+  <div class="banner">
+    <div class="banner-image">
+      <div class="container">
+        <div class="banner-holder">
+          <div class="bg-back">
+          </div>
+          <div class="bg-front">
+            <img src="images/banner-front.png" width="1028" height="310" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="banner-contents">
+      <div class="container">
+        <div class="banner-text">
+          <h1>
+            Peace of Mind Technology and Crestron deliver your amazing workplace experiences 
+          </h1>
+          <p>An exceptional and productive workplace experience goes beyond collaboration technology and AV equipment. It should instead encompass engagement, experience, change management, and the design of physical and virtual spaces better geared towards human behaviour.</p>
+          <div class="btn-holder">
+            <a href="#" class="btn">GET IN TOUCH</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="it-works" id="it-works">
+  <div class="container">
+    <div class="it-works-container">
+      <div class="it-works-left-panel">
+        <div class="it-works-content">
+          <h2>Just know it works</h2>
+          <p>Together, Peace Of Mind Technology (POMT) and Crestron bring you expertly engineered and supported unified communications and audio-visual solutions that will positively transform your team’s workplace experience.  </p>
+          <p>POMT provides you with an end-to-end service, including consultancy, implementation, and managed services, including enablement and adoption services.</p>
+          <div class="it-works-logo-content">
+             <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/crestron-middle-logo.png" width="177"  alt="Crestron Partners"></a>
+             <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/logo-pomt.svg" width="111" height="40px" alt="POMT"></a>
+          </div>
+        </div>
+      </div>
+      <div class="it-works-right-panel">
+        <img src="images/it-works-top.svg" alt="it-works-top" class="top-img">
+        <img src="images/it-works-main.png" alt="it-works-main" class="main-img">
+      </div>
+    </div>  
+  </div>
+</section>
+
+<section class="collaborate" id="collaborate" >
+  <div class="container">
+    <div class="collaborate-wrap">
+      <h2>Collaborate with confidence </h2>
+      <div class="collaborate-container">
+        <div class="collaborate-left">
+          <h3>Bring together people, spaces, and technology</h3>
+        </div>
+        <div class="collaborate-right">
+          <p>Work with POMT and Crestron to bring together your people, spaces, and technology. Our complete, end-to-end offering ensures you receive the highest level of service with all aspects of your solution integrating seamlessly for greater, more intuitive ease of use.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <img src="images/middle-red-line.svg" style="float:right;" class="middle-red-line">
+</section>
+
+<section class="performance" id="performance" >
+  <div class="container ">
+    <div class="performance-container">
+        <h2>We’re passionate about expert service</h2>
+      <div class="performance-contents ">
+          <div class="side">
+            <div class="img-wrap">
+              <img src="images/performance-first-img.svg">
+            </div>
+            <h5>Long term commitment  </h5>
+            <p>We’re honoured to have many long-term relationships with 85% customer retention because we’re passionate about truly helping our clients create amazing workspaces.
+            </p>
+
+          </div>
+          <div class="side">
+            <div class="img-wrap">
+              <img src="images/side-second.svg">
+            </div>
+            <h5>Leading partnerships</h5>
+            <p> POMT delivers lasting and measurable outcomes for leading global and local organisations including Atlassian, NIB Insurance, Fairfax, and TripAdvisor.
+            </p>
+
+          </div>
+          <div class="side">
+            <div class="img-wrap">
+              <img src="images/performance-last-img.svg">
+            </div>
+            <h5>Stands apart </h5>
+            <p> Distinct in market, POMT offers deeper consulting expertise, an end-to-end offering ensuring higher quality outcomes, and a people-focused culture.
+            </p>
+          </div>
+      </div>
+    </div> 
+  </div>
+</section>
+
+<section class="believe" id="believe">
+    <div class="container">
+      <div class="believe-container">
+        <div class="believe-left">
+          <h2>Believe in Crestron</h2>
+          <p>For more than 45 years, Crestron has helped businesses like yours with beautifully engineered automation, audio visual and unified communications equipment for modern workplaces.
+          </p> 
+          <p> Crestron technology simply works wonders, producing inspired collaboration you can believe in.</p>
+          <a class="btn">Find out more</a>
+        </div>
+        <div class="believe-right">
+          <div class="video-holder">
+            <iframe src="https://www.youtube.com/embed/amuH0Gs5ZhjhhjY" frameborder="0" style="    width: 663px;
+        height: 404px;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>  
+</section>
+
+<section class="work" id="work">
+  <div class="container">
+    <div class="work-container">
+
+      <img src="images/middle-red-line.svg"  class="red-left-line-works">
+      
+      <div class="work-form">
+        <h2>Work With Us</h2>
+         <p>Work with POMT and Crestron to deliver workspaces that help you attract and retain the best people while driving your organisation forward. Use the form to get in touch and book a <strong>free on-site demonstration</strong> at your office!</p>
+        <form action="form-submit.php" id="work-form" class="work-form">
+          <div class="form-group">
+            <div class="col">
+              <input type="text" name="firstname" placeholder="First Name" data-rule-required="true" data-msg-required="Please enter your first name">
+            </div>
+            <div class="col">
+              <input type="text" name="lastname" placeholder="Last Name" data-rule-required="true" data-msg-required="Please enter your last name">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col">
+              <input type="text" name="organization" placeholder="Organization">
+            </div>
+            <div class="col">
+              <input type="email" name="email" placeholder="Email" data-rule-required="true" data-msg-required="Please enter your email">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col">
+              <input type="tel" name="phone" placeholder="Phone" data-rule-minlength="10" data-rule-maxlength="10" data-rule-required="true" data-msg-required="Please enter your phone number">
+            </div>
+            <div class="col">
+              <select class="role" name="role">
+                <option selected value="Not specified">Role in Company</option>
+                <option selected value="CEO">CEO</option>
+                <option selected value="CTO">CTO</option>
+                <option selected value="Others">Others</option>
+              </select>
+              <!-- <input placeholder="Role in Company" type="text" class="role" list="roles" name="role">
+              <datalist id="roles">
+                <option value="CEO">
+                <option value="CTO">
+                <option value="Marketing Department">
+                <option value="Others">
+              </datalist> -->
+            </div>
+          </div>
+          <div class="form-group">
+            <textarea name="comments" placeholder="Comments"></textarea>
+          </div>
+          <div class="form-group btn-holder">
+            <input type="submit" class="btn btn-submit" value="submit">
+          </div>
+        </form>
+        <div class="message-success">
+          <p>Thank you for registering your interest. A member of our team will be in contact with you shortly.</p>
+        </div> <!-- end .message-success --> 
+      </div>
+
+      <div class="left-straight-block">
+      </div>
+
+    </div>
+  </div>
+</section>
---
+++ components/scripts/app.js
@@ -0,0 +1,75 @@
+'use strict';
+
+import $ from 'jquery';
+import AOS from 'aos';
+require ('jquery-validation');
+import jcf from 'jcf';
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('amr test');
+    console.log('test test');
+    AOS.init();
+
+    var $doc = $(document);
+
+
+    $doc.on("click", function(e){
+        var menu = $('#nav ul');
+        var menu_btn = $('#nav input');
+        // check that the click target is not the menu or it's child
+        if(!menu.is(e.target) && menu.has(e.target).length === 0){
+            // check if the target is the menu-toggle amd show or hide the menu accordingly
+            if(menu_btn.is(e.target)){
+                if ($(menu_btn).is(":checked")) {
+                    menu.fadeIn(300);
+                } else {
+                    menu.fadeOut(300);
+                }
+            }
+            // else if it's outside the menu or the menu-toggle, hide the menu and change the menu-toggle state
+            else{
+                menu.fadeOut(300);
+                menu_btn.prop("checked", false);
+            }
+        } 
+    });
+    $('.btn-submit').click(function(ev){
+        ev.preventDefault();
+        if( $("#work-form").valid() ){
+            $( ".btn-submit" ).addClass( "disabled" );
+            var var_data = $("#work-form").serialize(),
+                var_url = $("#work-form").attr('action');
+            $('body').css('cursor','wait');
+            $.ajax({
+                type:"POST",
+                url: var_url,
+                data: var_data,
+                success: function(response){
+                    $("#work-form").slideUp("slow",function(){
+                        $('.message-success').fadeIn(500);
+                    })
+                    $('body').css('cursor','auto');
+                }
+            });
+        }
+    });
+
+    var banner = $('.banner-image');
+    var bg = $('.bg-back');
+
+    $(document).mousemove(function(e){
+        var moveX=(e.pageX*-1/10);
+        var moveY=(e.pageY*-1/10);
+      
+        bg.css('background-position', moveX + 'px ' + moveY + 'px')
+        
+    })
+
+    jcf.setOptions('Select', {
+        wrapNative: false,
+        wrapNativeOnMobile: false,
+        fakeDropInBody: false,
+        maxVisibleItems: 5
+      });
+    jcf.replace($('.work-form select'));
+});
