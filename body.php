fefa026d8e6160ef498d337ed31ff67931043adc
Amar Shahukhal <amar.shahukhal@gmail.com>
2020-05-25 11:28:25 +1000

    copy and layout changes

--- body.php
+++ body.php
@@ -1,26 +1,9 @@
 <section class="top-page">
   <header id="header">
-    <!-- <div class="menu">
-      <div class="container">
-        <nav id="nav">
-          <input type="checkbox" id="show-mobile">
-          <span></span>
-          <span></span>
-          <span></span>
-          <ul>
-              <li><a href="#it-works">KNOW IT WORKS</a></li>
-              <li><a href="#collaborate">COLLABORATE WITH CONFIDENCE</a></li>
-              <li><a href="#performance">OUR PERFORMANCE</a></li>
-              <li><a href="#believe">BELIEVE IN CRESTRON</a></li>
-              <li><a href="#work">WORK WITH US</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div> -->
     <div class="logo-container">
       <div class="container">
-        <a href="https://www.crestron.com/" target="_blank"><img src="images/logo-crestron.png" width="177" alt="Crestron Partners"></a>
-        <a href="https://pomt.com/" target="_blank"><img src="images/logo-pomt.svg" width="111" alt="POMT"></a>
+        <a href="https://www.crestron.com/" target="_blank"><img src="https://storage.pardot.com/767943/36764/logo_crestron.png" width="177" alt="Crestron Partners"></a>
+        <a href="https://pomt.com" target="_blank"><img src="https://storage.pardot.com/767943/50894/logo_pomt.svg" width="111" alt="Ten Four"></a>
       </div>
     </div>
   </header>
@@ -28,24 +11,25 @@
     <div class="banner-image">
       <div class="container">
         <div class="banner-holder">
-          <div class="bg-back">
+          <div id="bgBack" class="bg-back" style="background-position: absolute;">
           </div>
           <div class="bg-front">
-            <img src="images/banner-front.png" width="1028" height="310" alt="">
+            <img src="https://storage.pardot.com/767943/36740/banner_front.png" width="1028" height="310" alt="">
           </div>
         </div>
       </div>
     </div>
-    
+
     <div class="banner-contents">
       <div class="container">
         <div class="banner-text">
           <h1>
-            Peace of Mind Technology and Crestron deliver your amazing workplace experiences 
+          POMT helps organisations to enable productivity and workplace experience via technology.
           </h1>
-          <p>An exceptional and productive workplace experience goes beyond collaboration technology and AV equipment. It should instead encompass engagement, experience, change management, and the design of physical and virtual spaces better geared towards human behaviour.</p>
+          <p>We do this through consultancy, implementation and managed services which bring together physical and virtual workspaces, platforms and people.</p>
+          <p>Modern organisations looking to maximise their return on technology investment and team performance are recognising the need for more than just the right hardware and software. How we enable and engage with our teams and help them leverage the technology and workplace experience will ultimately drive the best outcomes.</p>
           <div class="btn-holder">
-          <a href="#work" class="btn btn-anchor">GET IN TOUCH</a>
+            <a href="#work" class="btn btn-anchor">GET IN TOUCH <strong>WITH POMT</strong></a>
           </div>
         </div>
       </div>
@@ -59,23 +43,24 @@
       <div class="it-works-left-panel">
         <div class="it-works-content">
           <h2>Just know it works</h2>
-          <p>Together, Peace Of Mind Technology (POMT) and Crestron bring you expertly engineered and supported unified communications and audio-visual solutions that will positively transform your team’s workplace experience.  </p>
-          <p>POMT provides you with an end-to-end service, including consultancy, implementation, and managed services, including enablement and adoption services.</p>
+          <p>POMT leverages <strong>Crestron’s best-of-breed unified communications and audio visual solutions</strong> to deliver consistent, intuitive, high performance collaborative physical environments.</p>
+          <p>Crestron's range of hardware, combined with POMT’s expert design and deployment, ensures scalability with the right solution for each setting and environment securing a reliable and familiar user experience.</p>
+          <p>Crestron’s deployment and management tools are utilised by POMT to guarantee maximum up-time, security and flexibility in initial deployment and as part of ongoing managed services.</p>
           <div class="it-works-logo-content">
-             <a href="https://www.crestron.com/" target="_blank"><img src="images/crestron-middle-logo.png" width="177"  alt="Crestron Partners"></a>
-             <a href="https://pomt.com/" target="_blank"><img src="images/logo-pomt.svg" width="111" height="40px" alt="POMT"></a>
+             <a href="https://www.crestron.com/" target="_blank"><img src="https://go.crestroninfo.com/l/767943/2020-03-06/4lxx47/767943/36748/crestron_middle_logo.png" width="177" alt="Crestron Partners"></a>
+             <a href="https://pomt.com" target="_blank"><img src="https://storage.pardot.com/767943/50894/logo_pomt.svg" width="111" alt="Ten Four"></a>
           </div>
         </div>
       </div>
       <div class="it-works-right-panel">
-        <img src="images/it-works-top.svg" alt="it-works-top" class="top-img">
-        <img src="images/it-works-main.png" alt="it-works-main" class="main-img">
+<img src="https://go.crestroninfo.com/l/767943/2020-03-06/4lxx4m/767943/36760/it_works_top.svg" alt="it-works-top" class="top-img">
+        <img src="https://go.crestroninfo.com/l/767943/2020-03-06/4lxx4r/767943/36758/it_works_main.png" alt="it-works-main" class="main-img">
       </div>
-    </div>  
+    </div>
   </div>
 </section>
 
-<section class="collaborate" id="collaborate" >
+<section class="collaborate" id="collaborate">
   <div class="container">
     <div class="collaborate-wrap">
       <h2>Collaborate with confidence </h2>
@@ -84,47 +69,45 @@
           <h3>Bring together people, spaces, and technology</h3>
         </div>
         <div class="collaborate-right">
-          <p>Work with POMT and Crestron to bring together your people, spaces, and technology. Our complete, end-to-end offering ensures you receive the highest level of service with all aspects of your solution integrating seamlessly for greater, more intuitive ease of use.</p>
+          <p>POMT provides a complete, end-to-end offering from consultancy, through to implementation and ongoing managed services. This ensures we deliver on agreed outcomes and provide ultimate accountability for our services and solutions.</p>
+          <p>We take care of the physical and virtual technology, enabling productivity and workplace experience, allowing your organisation to focus on your business and customer offerings.</p>
         </div>
       </div>
     </div>
   </div>
-  <img src="images/middle-red-line.svg" style="float:right;" class="middle-red-line">
+<img src="https://go.crestroninfo.com/l/767943/2020-03-06/4lxx55/767943/36774/middle_red_line.svg" style="float:right;" class="middle-red-line">
 </section>
 
-<section class="performance" id="performance" >
+<section class="performance" id="performance">
   <div class="container ">
     <div class="performance-container">
-        <h2>We’re passionate about expert service</h2>
+        <h2>TRUSTED INDUSTRY LEADERS AND EXPERTS</h2>
       <div class="performance-contents ">
           <div class="side">
             <div class="img-wrap">
-              <img src="images/performance-first-img.svg">
-            </div>
-            <h5>Long term commitment  </h5>
-            <p>We’re honoured to have many long-term relationships with 85% customer retention because we’re passionate about truly helping our clients create amazing workspaces.
-            </p>
-
-          </div>
-          <div class="side">
-            <div class="img-wrap">
-              <img src="images/side-second.svg">
+              <img src="https://storage.pardot.com/767943/50904/icon_hand.svg">
             </div>
             <h5>Leading partnerships</h5>
-            <p> POMT delivers lasting and measurable outcomes for leading global and local organisations including Atlassian, NIB Insurance, Fairfax, and TripAdvisor.
-            </p>
+            <p>POMT is a certified partner for leading platforms including Microsoft Teams and Zoom, ensuring we can provide agnostic value-add services and solutions unmatched in the region.</p>
 
           </div>
           <div class="side">
             <div class="img-wrap">
-              <img src="images/performance-last-img.svg">
+              <img src="https://go.crestroninfo.com/l/767943/2020-03-06/4lxx5h/767943/36784/side_second.svg">
             </div>
-            <h5>Stands apart </h5>
-            <p> Distinct in market, POMT offers deeper consulting expertise, an end-to-end offering ensuring higher quality outcomes, and a people-focused culture.
-            </p>
+            <h5>Long term commitment</h5>
+            <p>We pride ourselves on a customer retention rate of over 86%, having built ongoing value-add relationships with leading organisations such as Atlassian, nib, BCG, Seek and Afterpay.</p>
+
+          </div>
+          <div class="side">
+            <div class="img-wrap">
+              <img src="https://storage.pardot.com/767943/50906/icon_star.svg">
+            </div>
+            <h5>Stands apart</h5>
+            <p>Our highly proficient consulting expertise, large scale delivery capability, and high touch managed services are unique in market.</p>
           </div>
       </div>
-    </div> 
+    </div>
   </div>
 </section>
 
@@ -135,30 +118,31 @@
           <div class="content-wrapper">
             <h2>Believe in Crestron</h2>
             <p>For more than 45 years, Crestron has helped businesses like yours with beautifully engineered automation, audio visual and unified communications equipment for modern workplaces.
-            </p> 
+            </p>
             <p> Crestron technology simply works wonders, producing inspired collaboration you can believe in.</p>
-            <a class="btn" target="_blank" href="https://believeincrestron.com">Find out more</a>
+            <a class="btn" target="_blank" href="https://believeincrestron.com/">Find out more ABOUT CRESTRON</a>
           </div>
         </div>
         <div class="believe-right">
           <div class="video-holder">
-          <iframe src="https://www.youtube.com/embed/amuH0Gs5ZhjhhjY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/hPGonbwewHc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
           </div>
         </div>
       </div>
-    </div>  
+    </div>
 </section>
 
 <section class="work" id="work">
   <div class="container">
     <div class="work-container">
+<img src="https://go.crestroninfo.com/l/767943/2020-03-06/4lxx55/767943/36774/middle_red_line.svg" class="red-left-line-works">
+      <div class="work-form"><a name="work"></a>
+        <div class="work-form-content-wrapper">
+        <h2>Work With POMT</h2>
+        <p>Work with POMT to deliver workspaces that help you attract and retain the best people while driving your organisation forward. Complete the form below to <strong>schedule a time to visit our workplace experience centre</strong> or for POMT to come to you!</p>
+        </div>
 
-      <img src="images/middle-red-line.svg"  class="red-left-line-works">
-      
-      <div class="work-form">
-        <h2>Work With Us</h2>
-         <p>Work with POMT and Crestron to deliver workspaces that help you attract and retain the best people while driving your organisation forward. Use the form to get in touch and book a <strong>free on-site demonstration</strong> at your office!</p>
-        <form action="form-submit.php" id="work-form" class="work-form">
+        <form action="http://crestron-tenfour.ba-uat.com.au/form-submit.php" id="work-form" class="work-form">
           <div class="form-group">
             <div class="col">
               <input type="text" name="firstname" placeholder="First Name" data-rule-required="true" data-msg-required="Please enter your first name">
@@ -182,9 +166,9 @@
             <div class="col">
               <!-- <select class="role" name="role">
                 <option selected value="Not specified">Role in Company</option>
-                <option selected value="CEO">CEO</option>
-                <option selected value="CTO">CTO</option>
-                <option selected value="Others">Others</option>
+                <option  value="CEO">CEO</option>
+                <option  value="CTO">CTO</option>
+                <option  value="Others">Others</option>
               </select> -->
               <!-- <input placeholder="Role in Company" type="text" class="role" list="roles" name="role">
               <datalist id="roles">
@@ -201,13 +185,13 @@
           <div class="form-group btn-holder">
             <input type="submit" class="btn btn-submit" value="submit">
           </div>
-          <div class="form-group terms">
-          <p>By submitting your information you consent to the use of that information as set out in the <a href="https://www.crestron.com/legal/website-terms_of_use" target="_blank">Terms of Use</a> and <a href="https://www.crestron.com/legal/privacy-policy" target="_blank">Privacy Policy</a></p>
-          </div>
         </form>
+        <div class="form-group terms">
+          <p>By submitting your information you consent to the use of that information as set out in the <a href="https://www.crestron.com/legal/website-terms_of_use" target="_blank">Terms of Use</a> and <a href="https://www.crestron.com/legal/privacy-policy" target="_blank">Privacy Policy</a></p>
+        </div>
         <div class="message-success">
           <p>Thanks for reaching out to us! A local Crestron expert will be in touch with you shortly.</p>
-        </div> <!-- end .message-success --> 
+        </div> <!-- end .message-success -->
       </div>
 
       <div class="left-straight-block">
@@ -222,8 +206,8 @@
 <div class="container">
   <div class="middle-logo">
     <div class="left-footer">
-        <a href="https://www.crestron.com/" target="_blank"><img src="images/logo-crestron.png" width="177" alt="Crestron Partners"></a>
-        <a href="https://pomt.com/" target="_blank"><img src="images/logo-pomt.svg" width="111" alt="POMT"></a>
+        <a href="https://www.crestron.com/" target="_blank"><img src="https://go.crestroninfo.com/l/767943/2020-03-06/4lxx4t/767943/36764/logo_crestron.png" width="177" alt="Crestron Partners"></a>
+        <a href="https://pomt.com" target="_blank"><img src="https://storage.pardot.com/767943/50894/logo_pomt.svg" width="111" alt="Ten Four"></a>
     </div>
-  </div>  
+  </div>
 </div>
--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$LPname = "Crestron - Report";
+/* $LPname = "Crestron - Report";
 
 $conn = new mysqli('localhost','root', 'Bang2020!', 'crestron_pomt'); 
 if ($conn->connect_error) {
     trigger_error('Database connection failed: ' . $conn->connect_error, E_USER_ERROR);
-}
+} */
 ?>
